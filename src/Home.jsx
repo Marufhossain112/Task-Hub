c7add5a444f36a9f0a50a8e8952bf62198aa8c00
Maruf Hossain <dev.maruf112@gmail.com>
2024-02-08 14:28:05 +0600

    project initial setup done

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+
+  return (
+    <>
+      <div>
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
+    </>
+  );
+}
+
+export default Home;
